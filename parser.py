e35b2388c197f4640ca9bc61b985d67d7988c1ef
ai-rage <azamat.murdalov@mail.ru>
2018-12-05 22:37:37 +0300

    update parser.py

--- parser.py
+++ parser.py
@@ -8,8 +8,25 @@ def get_html(url):
 	response = urllib.request.urlopen(req)
 	return response.read()
 
+def parse(html):
+	soup = BeautifulSoup(html)
+	main = soup.find('main', class_='site-main')
+
+	article_list = []
+
+	for row in main.find_all('article'):
+		title = row.find_all('header')
+		content = row.find_all('div')
+		article_list.append({
+			'title': title[0].h2.a.text,
+			'content': [ content.text for content in content[0].find_all('p')]
+		})
+
+	for article in article_list:
+		print(article)
+
 def main():
-	print(get_html('http://murdalov.ru'))
+	parse(get_html('http://murdalov.ru'))
 
 if __name__ == '__main__':
 	main()
